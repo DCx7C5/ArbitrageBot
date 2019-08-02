b50cf29d24ae1b96ce934ffbf2be0ee8ed723011
dcx7c5 <greenstreetcryptoelite@gmail.com>
2019-08-02 06:11:47 +0200

    Added account and orders function

--- botlib/graviex.py
+++ botlib/graviex.py
@@ -9,6 +9,8 @@ BASEURL = 'https://graviex.net/api/v3'
 # API ENDPOINTS
 MARKETS = '/markets'
 TICKERS = '/tickers'
+ACCOUNT = '/account/history'
+ORDERS = '/orders'
 
 # REQUEST METHODS
 PUT = "PUT"
@@ -26,57 +28,82 @@ class GraviexClient:
         self.api_key = api_key
         self.api_secret = api_secret
 
+    def _generate_request_string(self, params):
+        params_string = ""
+        for p in params:
+            params_string += f'&{p}={params[p]}'
+        return f'access_key={self.api_key}{params_string}&tonce={int(time.time() * 1000)}'
+
+    def _generate_message_string(self, method, endpoint, params):
+        req_string = self._generate_request_string(params=params)
+        return f'{method}|/api/v3{endpoint}|{req_string}'
+
     def _generate_hash_signature(self, msg_string):
         return hmac.new(key=self.api_secret.encode(),
                         msg=msg_string.encode(),
                         digestmod=hashlib.sha256).hexdigest()
 
-    def _api_call(self, endpoint: str,
-                  method: str,
+    def _api_call(self, method: str,
+                  endpoint: str,
                   params: dict = None):
         """
         BASE FUNCTION FOR MAKING API CALLS TO GRAVIEX
         """
-        params_req_str = ""
         if not params:
-            req_string = f'access_key={self.api_key}&tonce={int(time.time() * 1000)}'
+            params = {}
         else:
-            for i in params:
-                params_req_str += f'&{i}={params[i]}'
-            req_string = f'access_key={self.api_key}{params_req_str}&tonce={int(time.time() * 1000)}'
-
-        if method is PUT:
-            request = requests.put
-        elif method is DELETE:
-            request = requests.delete
-        elif method is POST:
-            request = requests.post
-        elif method is GET:
-            request = requests.get
-        else:
-            request = requests.get
-
-        msg_string = f'{method}|/api/v3{endpoint}|{req_string}'
+            params = params
+        
+        req_string = self._generate_request_string(params=params)
+        print(req_string)
+        msg_string = self._generate_message_string(method=method, endpoint=endpoint, params=params)
 
         signature = self._generate_hash_signature(msg_string)
 
         url = f'{BASEURL}{endpoint}?{req_string}&signature={signature}'
 
-        response = request(url, params=params)
+        params.update({'signature': signature})
 
+        if method is POST:
+            request = requests.post
+        else:
+            request = requests.get
+        print(url)
+        print(params)
+        response = request(url, params=params)
+        print(response.text)
         assert response.status_code is 200
 
         return response.json()
 
+    def _list_orders(self, market_id=None):
+        if market_id is None:
+            return self._api_call(endpoint=ORDERS, method=GET)
+        return self._api_call(
+            endpoint=ORDERS,
+            method=GET,
+            params={'market': market_id}
+        )
+
     def _list_markets(self, market_id=None):
         if market_id is None:
             return self._api_call(endpoint=MARKETS, method=GET)
-        return self._api_call(endpoint=MARKETS+"/"+market_id, method=GET, params={'market': market_id})
+        return self._api_call(
+            endpoint=MARKETS+"/"+market_id,
+            method=GET,
+            params={'market': market_id}
+        )
 
     def _list_tickers(self, market_id=None):
         if market_id is None:
             return self._api_call(endpoint=TICKERS, method=GET)
         return self._api_call(endpoint=TICKERS+"/"+market_id, method=GET, params={'market': market_id})
+
+    def _list_account(self, **kwargs):
+        params = {}
+        for kw in kwargs:
+            params.update({kw: kwargs[kw]})
+        return self._api_call(endpoint=ACCOUNT, method=GET, params=params)
 
     def get_all_coin_tickers(self) -> dict:
         """
@@ -107,3 +134,6 @@ class GraviexClient:
         Returns a dictionary with with market infos for specific pair
         """
         return self._list_markets(market_id)
+
+    def get_account_info(self):
+        return self._list_account()
