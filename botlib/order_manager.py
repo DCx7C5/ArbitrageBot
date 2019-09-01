b0f6c230e8f7ecec58c63e4489931b3e219ab19a
dcx7c5 <greenstreetcryptoelite@gmail.com>
2019-09-01 17:20:55 +0200

    More updates in order_manager daemon

--- botlib/order_manager.py
+++ botlib/order_manager.py
@@ -1,24 +1,36 @@
-import random
 import time
 from queue import Queue
 from threading import Lock, Thread
-from collections import Counter
 
 from botlib.bot_log import daemon_logger
 
-# Create child from root logger for OrderBook Daemon
+# Create child from daemon_logger for Jobs Daemon
 od_logger = daemon_logger.getChild('OrderManager')
 
+# Create child from od_logger for Jobs Daemon
+aj_logger = od_logger.getChild('Job')
 
-class CreateOrder(Thread):
 
-    def __init__(self):
+class ArbitrageJob(Thread):
+
+    def __init__(self, job):
         Thread.__init__(self)
+        self.__job = job
+        self.name = f'ArbitrageJob'
+
+    def do_sell_order(self):
+        pass
+
+    def do_buy_order(self):
+        pass
+
+    def run(self) -> None:
+        pass
 
 
 class OrderManagerDaemon(Thread):
 
-    def __init__(self):
+    def __init__(self, ):
         Thread.__init__(self)
         self.daemon = True
         self.name = 'JobsOrdersSync'
@@ -27,14 +39,19 @@ class OrderManagerDaemon(Thread):
         self.__logger = od_logger
         self.__last_log = time.time()
 
+    def add_to_job_queue(self, job):
+        with self.__lock:
+            self.__job_queue.put(job)
+
     def run(self) -> None:
         self.__logger.info('Daemon started!')
         while True:
             if self.__job_queue.empty():
                 time.sleep(.5)
                 continue
-            args = self.__job_queue.get()
+            job = self.__job_queue.get()
+            ArbitrageJob(job).start()
             if time.time() > self.__last_log + 20:
-                self.__logger.debug(f'Waiting for orders...')
+                self.__logger.debug(f'Waiting for jobs...')
                 self.__last_log = time.time()
-            time.sleep(.25)
+            time.sleep(.5)
